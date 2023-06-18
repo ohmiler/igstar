09af86180241102b64d1e3221d18604224a41717
Patiphan Phengpao <misterpatipan123@gmail.com>
2023-06-18 13:13:21 +0700

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,13 +10,13 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAL0UMj7dOU3rXFcGREbq2KuHMl2j3LyRY",
-  authDomain: "react-ig-clone-91d4a.firebaseapp.com",
-  projectId: "react-ig-clone-91d4a",
-  storageBucket: "react-ig-clone-91d4a.appspot.com",
-  messagingSenderId: "832341734959",
-  appId: "1:832341734959:web:1da7bb4619f68922d3f8da",
-  measurementId: "G-RYSSKMMXZF"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
